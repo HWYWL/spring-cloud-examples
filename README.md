f3436d63b50329106297cefaa335276132766d00
YI <18819478773@163.com>
2018-04-17 14:57:50 +0800

    使用Feign消费服务并实现负载均衡

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Spring Cloud 学习例程
 - 服务的注册和发现------->spring-cloud-eureka
 - Consul服务治理实现----->spring-cloud-consul
 - 使用Ribbon实现负载均衡，服务提供者 Eureka + 服务消费者（rest + Ribbon）---->spring-cloud-ribbon
+- 使用Feign消费服务并实现负载均衡------>spring-cloud-feign
---
+++ spring-cloud-feign/spring-cloud-eureka-service/src/main/java/com/yi/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.yi.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 提供服务注册和发现
+ * @author YI
+ * @date 2018-4-17 11:31:05
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
