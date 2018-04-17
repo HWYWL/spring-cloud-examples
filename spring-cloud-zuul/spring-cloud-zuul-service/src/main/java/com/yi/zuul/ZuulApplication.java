149b10a1161ac656009d1e3d9a568f1768e5ef41
YI <18819478773@163.com>
2018-04-17 18:30:46 +0800

    zuul网关基础

---
+++ spring-cloud-zuul/spring-cloud-zuul-service/src/main/java/com/yi/zuul/ZuulApplication.java
@@ -0,0 +1,21 @@
+package com.yi.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * zuul网关
+ * @author YI
+ * @date 2018-4-17 17:43:11
+ */
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
