21bb4c21cfbaf504f62e124634225d0cb868e2dd
YI <18819478773@163.com>
2018-04-18 09:44:43 +0800

    spring-cloud-config-server 配置，使用git存储配置文件

---
+++ spring-cloud-config/spring-cloud-config-server/src/main/java/com/yi/config/server/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.yi.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Config Server服务端
+ * @author YI
+ * @date 2018-4-18 09:31:21
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
