bed3122cc57a3f2e792e0c20c90be7dd4d91356d
YI <18819478773@163.com>
2018-04-18 11:47:01 +0800

    #(#^.^#)

--- spring-cloud-config/spring-cloud-config-server/src/main/java/com/yi/config/server/ConfigServerApplication.java
+++ spring-cloud-config/spring-cloud-config-server/src/main/java/com/yi/config/server/ConfigServerApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Config Server服务端
- * http://localhost:8888/springCloudConfig/pre/master
- * http://localhost:8888/springCloudConfig/dev/master
- * http://localhost:8888/springCloudConfig/test/master
+ * http://localhost:8888/springCloudConfig/pre
+ * http://localhost:8888/springCloudConfig/dev
+ * http://localhost:8888/springCloudConfig/test
  * @author YI
  * @date 2018-4-18 09:31:21
  */
